f5c2ed50118a52eaa75d227109287381c052b919
nthi2003 <quocthi.dev@gmail.com>
2024-03-11 22:27:48 +0700

    update #2

--- bai_tap.js
+++ bai_tap.js
@@ -53,3 +53,114 @@ const myDog = new Dog('Kiki','Gâu', 'Chó');
 myDog.introduce()
 myDog.talk()
 myDog.DongVat()
+
+// function Car(){
+//    this.speed =  0;
+// }
+
+// Car.prototype = {
+//     constructor: Car,
+//     printSpeed: function(){
+//        console.log(this.speed+'<br/>')
+//     },
+// }
+
+// var car = new Car()
+//     car.instanceMethod = function() {
+//         console.log('An instance method <br/>')
+//     }
+// car.speed = 50;
+// car.printSpeed();
+// car.instanceMethod();
+// function Cabriolet(){}
+//     Cabriolet.prototype = new Car();
+//     Cabriolet.prototype.topState = 'colosed'
+//     Cabriolet.prototype.openTop = function(){
+//         this.topState = 'open';
+//         console.log(this.topState)
+//     }
+//     Cabriolet.prototype.closeTop = function(){
+//         this.topState = 'close';
+//         console.log(this.topState)
+//     }
+
+//     var cabriolet = new Cabriolet();
+//     cabriolet.speed = 120;
+//     cabriolet.printSpeed();
+//     cabriolet.openTop();
+//     cabriolet.closeTop();
+//     function TeslaCabriolet(){}
+//     TeslaCabriolet.prototype = new Cabriolet();
+//     TeslaCabriolet.prototype.name = 'Tesla http'
+//     TeslaCabriolet.prototype.sayName = function(){
+//         console.log(this.name);
+//     }
+//     var tesla = new TeslaCabriolet()
+//     tesla.openTop()
+//     tesla.sayName()
+
+
+class Car {
+    constructor(speed, user) {
+        this.speed = speed;
+        this.user = user;
+
+    }
+    printSpeed(){
+        console.log(`Current speed: ${this.speed}`);
+    }
+    instanceMethod() {
+        console.log('An instance method <br/>')
+    }
+}
+
+class Cabriolet extends Car {
+    constructor(){
+       super()
+       this.speed = 'closed'
+    }
+    openTop() {
+        this.topState = 'opened';
+        console.log('Top: ' + this.topState + '<br/>');
+    }
+    closeTop(){
+        this.topState = 'closed';
+        console.log('Top: ' + this.topState+ '<br/>');
+    }
+}
+
+
+
+ class TeslaCabriolet extends Cabriolet {
+    constructor() {
+        super();
+        this.name = 'Tesla http/// ....'; 
+    }
+
+    openTop() {
+        super.openTop();
+    }
+
+
+    sayName() {
+        console.log(this.name);
+    }
+}
+const car = new Car();
+car.speed = 50;
+car.printSpeed();
+
+car.instanceMethod = function() {
+    console.log('An instance method <br/>');
+}
+car.instanceMethod();
+
+const cabriolet = new Cabriolet();
+cabriolet.speed = 120;
+cabriolet.printSpeed();
+cabriolet.openTop();
+cabriolet.closeTop();
+const tesla = new TeslaCabriolet();
+tesla.openTop();
+tesla.sayName();
+
